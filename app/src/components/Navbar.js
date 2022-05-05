26db1d4b7c6d26f932b3b136183d855311463de3
Melanie Chan <melanie.chan@sjsu.edu>
2022-05-04 23:15:46 -0700

    used object to organize navbar path data

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -4,23 +4,43 @@ import './Navbar.css';
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  let navigate = useNavigate()
+  let navigate = useNavigate() // used to change pages
+
+  var currentPath = window.location.pathname; // suffix of current link url
+
+  // names of pages and their paths
+  const paths = [
+    {name: 'My Pantry', route: '/pantry'},
+    {name: 'Search Recipe', route: '/search'},
+    {name: 'My Cookbook', route: '/search'},
+  ]
+
+  const changeRoute = (route) => {
+    // goes to new page if the route is different
+    if (route !== currentPath)
+      navigate(route)
+  }
 
   return(
     <>
       <div className="header overlay">
         <div className="left-header">
           <h1 id="title">Recipe App</h1>
-          <p className="tab-title"
-            onClick={() => navigate('/pantry')}>My Pantry</p>
-          <p className="tab-title"
-            onClick={() => navigate('/search')}>Search Recipe</p>
-          <p className="tab-title">My Recipes</p>
+
+          {/* horizontal row of tabs that user can click on to go to a different page */}
+          {paths.map(path =>
+            <p
+              // if current path is the same as what this path links to, it will be disabled
+              className={path.route === currentPath ? 'tab-current' : "tab-title"}
+              onClick={() => changeRoute(path.route)}
+              >{path.name}</p>
+          )}
         </div>
 
         <button>Log in</button>
       </div>
 
+      {/* this div has no real functionality, it is only to fix styling */}
       <div className="header">
         <div className="left-header">
           <h1 id="title">X</h1>
