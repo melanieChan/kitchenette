edec88b858f91b93a379f02802452d0a821e1e83
Melanie Chan <melanie.chan@sjsu.edu>
2022-05-09 16:16:58 -0700

    displayed recipe data on recipe card

--- app/src/components/cards/RecipeCard.js
+++ app/src/components/cards/RecipeCard.js
@@ -1,12 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 // A card showing details about a recipe
 const RecipeCard = ({recipe}) => {
+  const [instructionsList, setInstructionsList] = useState(null) // list of strings representing list of instructions
+
+  useEffect(() => {
+    // process instructions list
+    // remove these 2 characters that may surround some words: \"
+    // turn into array of steps
+    setInstructionsList(recipe.instructions.replace('\\"','').split("\", \""))
+  }, [recipe])
+
   return (
     <div className="section recipe-card" key={recipe.name}>
       <h1>{recipe.name}</h1>
-      <p>ingredient list</p>
-      <p>step-by-step instructions</p>
+
+      <ul> {/* ingredient list */}
+        <p>Ingredients</p>
+        {recipe.ingredients &&
+          recipe.ingredients.map(ingredient => <li>{ingredient}</li>)
+        }
+      </ul>
+
+      <ol>
+        <p>Instructions</p>
+        {instructionsList && instructionsList.map(step => <li>{step}</li>)}
+      </ol>
+
       <div>
         <button className="button">save</button>
       </div>
