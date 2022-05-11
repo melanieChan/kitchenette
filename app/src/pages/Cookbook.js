bcfafc2c99fa79955ec7e5dc835e0784f6afc8b8
Melanie Chan <melanie.chan@sjsu.edu>
2022-05-11 16:09:42 -0700

    fixed cookbook recipe page deletion bug
    - disabled page turning if there was only 1 recipe page
    - showed placeholder content if there were no recipes saved
    - fixed page numbering bug after array was shortened and indices shifted, by resetting the page number when the last page gets deleted (so the page number wouldn't be out of range in the new smaller array)
    - rounded corners on image and recipe page

--- app/src/pages/Cookbook.js
+++ app/src/pages/Cookbook.js
@@ -33,7 +33,7 @@ const Cookbook = () => {
   }, [])
 
   async function turnPage() {
-    setPageNum((pageNum + 1) % recipeSearchResults.length)
+    setPageNum((pageNum) => (pageNum + 1) % recipeSearchResults.length)
     // currentPageRef.current.classList.remove('flip')
   }
 
@@ -60,11 +60,22 @@ const Cookbook = () => {
         console.log(response);
 
         // update UI to remove recipe from displayed list
-        alert('Unsaved. The update will be applied in the next page load.');
-        // turnPage()
-        // setRecipeSearchResults(recipeSearchResults.filter(recipe => recipe.recipe_id !== unsaved_recipe_id))
+        // after deletion, it will show the next item in list that comes after the deleted item
+        // if deleting last item in list, next page shown will be first item
+        if (pageNum === recipeSearchResults.length - 1) setPageNum(pageNum => 0)
+        setRecipeSearchResults(recipeSearchResults.filter(recipe => recipe.recipe_id !== unsaved_recipe_id))
       })
       .catch(err => { console.log(err) });
+  }
+
+  // if there're no recipes saved
+  if (!recipeSearchResults || recipeSearchResults.length === 0) {
+    return (
+      <div id="content" className="page-content center">
+        <div style={{maxWidth: '50%'}}><img src={defaultImage}/></div>
+        <h2 className="salmon-text">No recipes saved! Save a few through the Search page to see them in your cookbook.</h2>
+      </div>
+    );
   }
 
   return (
@@ -78,40 +89,51 @@ const Cookbook = () => {
           display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'
         }}>
 
-        <div className="center" style={{width: '30%', minWidth: '300px'}} key={pageNum}>
-            {recipeSearchResults && recipeSearchResults.length > 0 && pageNum >= 0 &&
-            <Image src={recipeSearchResults && recipeSearchResults[pageNum] && recipeSearchResults[pageNum].image}/>}
-        </div>
+        {recipeSearchResults[pageNum] && pageNum >= 0 &&
+        /* image of the recipe */
+        <div className="center" style={{width: '30%', minWidth: '300px'}} key={Math.random()}>
+            <Image src={recipeSearchResults[pageNum] && recipeSearchResults[pageNum].image}/>
+        </div>}
 
-        {/* contains cookbook pages */}
-        <div style={{position: 'relative', minWidth: '600px', marginLeft: '30px'}}>
-          {/* top page */}
-          <div className="flippable" ref={currentPageRef} key={pageNum}
-            style={{position:'absolute', zIndex: 2}}>
-            {recipeSearchResults && (
-              <RecipePaper recipe={recipeSearchResults[pageNum]}
-                onClickUnsave={onClickUnsave}
-                />
-            )}
+        <div style={{position: 'relative', minWidth: '600px', marginLeft: '30px'}} key={Math.random()}>
+        {recipeSearchResults.length > 1 && pageNum >= 0 ?
+          <> {/* stack of cookbook pages */}
+            {/* top page */}
+            <div className="flippable" ref={currentPageRef}
+              style={{position:'absolute', zIndex: 2}}>
+              {recipeSearchResults && (
+                <RecipePaper
+                  recipe={recipeSearchResults[pageNum]}
+                  onClickUnsave={onClickUnsave}
+                  />
+              )}
+            </div>
+
+            {/* 2nd page */}
+            <div style={{position:'absolute', zIndex: 1}}>
+              {recipeSearchResults && <RecipePaper recipe={recipeSearchResults[(pageNum + 1) % recipeSearchResults.length]}/>}
+            </div>
+          </> :
+          /* only shows page if there's 1 recipe saved */
+          <div className="flippable" style={{position:'absolute', zIndex: 1}}>
+            <RecipePaper
+              recipe={recipeSearchResults[(pageNum) % recipeSearchResults.length]}
+              onClickUnsave={onClickUnsave}
+              />
           </div>
+        }
+        </div>
 
-          {/* 2nd page */}
-          <div style={{position:'absolute', zIndex: 1}}>
-            {recipeSearchResults && <RecipePaper recipe={recipeSearchResults[(pageNum + 1) % recipeSearchResults.length]}/>}
+        {/* next button */
+          recipeSearchResults.length > 1 &&
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <button className="button" onClick={() => {
+              // flip page
+                  currentPageRef.current.classList.add('flip'); // animate current page turning
+                  setTimeout(()=>{turnPage()}, 1500) // go to next page after 1.2 seconds
+            }}>next ></button>
           </div>
-        </div>
-
-        {/* next button */}
-        <div style={{display: 'flex', alignItems: 'center'}}>
-          <button className="button" onClick={() => {
-            // flip page
-            var promise = Promise.resolve('promise resolved');
-            promise.then(() => {
-                currentPageRef.current.classList.add('flip'); // animate current page turning
-                setTimeout(()=>{turnPage()}, 1200) // go to next page after 1.2 seconds
-            })
-          }}>next ></button>
-        </div>
+        }
       </div>
     </div>
   );
